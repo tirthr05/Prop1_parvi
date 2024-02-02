fb82ef575c6ebea32290a53f7cbb56f0e2d4d129
tirthr05 <92134664+tirthr05@users.noreply.github.com>
2024-02-02 12:51:49 -0500

    Add files via upload

---
+++ Prop 1_ FUN/script.js
@@ -0,0 +1,35 @@
+
+var love = setInterval(function(){
+  var r_num = Math.floor(Math.random() * 40) + 1;
+	var r_size = Math.floor(Math.random() * 65) + 10;
+	var r_left = Math.floor(Math.random() * 100) + 1;
+	var r_bg = Math.floor(Math.random() * 25) + 100;
+  var r_time = Math.floor(Math.random() * 5) + 5;
+  $('.bg_heart').append("<div class='heart' style='width:"+r_size+"px;height:"+r_size+"px;left:"+r_left+"%;background:rgba(255,"+(r_bg-25)+","+r_bg+",1);-webkit-animation:love "+r_time+"s ease;-moz-animation:love "+r_time+"s ease;-ms-animation:love "+r_time+"s ease;animation:love "+r_time+"s ease'></div>");
+  
+  $('.bg_heart').append("<div class='heart' style='width:"+(r_size-10)+"px;height:"+(r_size-10)+"px;left:"+(r_left+r_num)+"%;background:rgba(255,"+(r_bg-25)+","+(r_bg+25)+",1);-webkit-animation:love "+(r_time+5)+"s ease;-moz-animation:love "+(r_time+5)+"s ease;-ms-animation:love "+(r_time+5)+"s ease;animation:love "+(r_time+5)+"s ease'></div>");
+  
+  
+},500);
+
+var i = 0;
+var txt1 = "Hi MY LOVE OF MY LIFE(LIFELINE)🌏.....!  <<               Now I wan't to say something special to you. <<<                So , Please read everything carefully...!                                                                           > When I saw you for the first time < You seems something Special to me.  <<                  As the days goes < you get closer to me....! <<                           I don't know the reason why your thoughts always resonates inside my mind...!                                                     > Everything about you is always intresting for me...!                     << I am somewhat nervous Because I haven't said these words to anyone and I won't say to anyone in future...!                                                     > I Love my Parents so much than anything else in this world....!                    << Now You are the only person  whom I love equally with my parents....!                                                             > I am irrevocably, completely, and <wholeheartedly in love with you..!  I LOVE YOU!!! WILL PRAY THAT THIS JOURNEY WILL CONTINUE AND IT LEADS TO MARRIAGE!!🥰🥰🥰 |                  <<<< If I were a stop light🚦, I would turn red every💓 time you passed by, so that I could stare at you a bit longer...!  💕🌹😘👌👸";
+var speed = 50;
+typeWriter();
+function typeWriter() {
+  if (i < txt1.length) {        
+     if(txt1.charAt(i)=='<')
+      document.getElementById("text1").innerHTML += '</br>'
+    else if(txt1.charAt(i)=='>')
+      document.getElementById("text1").innerHTML = ''
+    else if(txt1.charAt(i)=='|')
+      {
+        $(".bg_heart").css("https://tminspired.com/wp-content/uploads/2020/03/Sunset-Cliffs-Engagement-Photographer-TMinspired-Photography-San-Diego-59.jpg')");
+
+      }
+    else
+      document.getElementById("text1").innerHTML += txt1.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
